3db2873191896b97ef1ee29308c9e0c2a9df02bc
Mark Heath <mark.heath@gmail.com>
2022-12-06 07:19:00 +0000

    day 6 part 2

--- AdventOfCode2022/Day6.cs
+++ AdventOfCode2022/Day6.cs
@@ -4,17 +4,17 @@ namespace AdventOfCode2022;
 
 public class Day6 : ISolver
 {
-    public (string, string) ExpectedResult => ("1892", "");
+    public (string, string) ExpectedResult => ("1892", "2313");
 
-    public int FindMarkerPosition(string input) =>
-        input.Window(4).TakeUntil(w => w.ToHashSet().Count == 4).Count() + 3;
+    public int FindMarkerPosition(string input, int distinct) =>
+        input.Window(distinct).TakeUntil(w => w.ToHashSet().Count == distinct).Count() + distinct - 1;
 
     public (string, string) Solve(string[] input)
     {
         return ($"{Part1(input)}", $"{Part2(input)}");
     }
 
-    long Part1(IEnumerable<string> input) => FindMarkerPosition(input.First());
-    long Part2(IEnumerable<string> input) => 0;
+    long Part1(IEnumerable<string> input) => FindMarkerPosition(input.First(), 4);
+    long Part2(IEnumerable<string> input) => FindMarkerPosition(input.First(), 14);
 
 }
--- UnitTests/Day6Tests.cs
+++ UnitTests/Day6Tests.cs
@@ -6,16 +6,20 @@ namespace UnitTests;
 
 public class Day6Tests
 {
-    [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
-    [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz",5)]
-    [TestCase("nppdvjthqldpwncqszvftbrmjlhg", 6)]
-    [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",10)]
-    [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
-
-    public void Part1TestCases(string input, int expectedPosition)
+    [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7, 4)]
+    [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz",5, 4)]
+    [TestCase("nppdvjthqldpwncqszvftbrmjlhg", 6, 4)]
+    [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",10, 4)]
+    [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11, 4)]
+    [TestCase("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19, 14)]
+    [TestCase("bvwbjplbgvbhsrlpgdmjqwftvncz", 23, 14)]
+    [TestCase("nppdvjthqldpwncqszvftbrmjlhg", 23, 14)]
+    [TestCase("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29, 14)]
+    [TestCase("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26, 14)]
+    public void Day6TestCases(string input, int expectedPosition, int distinct)
     {
         var solver = new Day6();
-        var solution = solver.FindMarkerPosition(input);
+        var solution = solver.FindMarkerPosition(input, distinct);
         Assert.AreEqual(expectedPosition, solution);
     }
 }
