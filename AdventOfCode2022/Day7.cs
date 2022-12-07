e75dd5e4740c34aa0e9a0dff96d555d44b7fe447
Mark Heath <mark.heath@gmail.com>
2022-12-07 07:12:35 +0000

    day 7 part 1

--- AdventOfCode2022/Day7.cs
+++ AdventOfCode2022/Day7.cs
@@ -1,15 +1,96 @@
-﻿namespace AdventOfCode2022;
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2022;
 
 public class Day7 : ISolver
 {
-    public (string, string) ExpectedResult => ("", "");
+    public (string, string) ExpectedResult => ("1778099", "");
 
     public (string, string) Solve(string[] input)
     {
         return ($"{Part1(input)}", $"{Part2(input)}");
     }
 
-    long Part1(IEnumerable<string> input) => throw new NotImplementedException();
-    long Part2(IEnumerable<string> input) => throw new NotImplementedException();
+    public record File(string Name, long Size);
+    public class Folder
+    {
+        public List<Folder> Folders { get; }
+        public List<File> Files { get; }
+        public string Name { get; }
+        public Folder Parent { get; }
+
+        public Folder(string name, Folder parent)
+        {
+            Files = new List<File>();
+            Folders = new List<Folder>();
+            Name = name;
+            Parent = parent;
+        }
+
+        public long GetSize()
+        {
+            return Files.Sum(f => f.Size)
+                   + Folders.Sum(f => f.GetSize());
+        }
+
+        public IEnumerable<Folder> GetChildFolders()
+        {
+            foreach(var f in Folders)
+            {
+                yield return f;
+                foreach(var child in f.GetChildFolders())
+                {
+                    yield return child;
+                }
+            }
+        }
+    }
+
+    public Folder ParseInput(IEnumerable<string> input)
+    {
+        Folder currentFolder = null;
+        foreach (var line in input)
+        {
+            if (line.StartsWith("$ cd"))
+            {
+                if (line.EndsWith(".."))
+                {
+                    currentFolder = currentFolder.Parent;
+                }
+                else
+                {
+                    var f = new Folder(line[5..], currentFolder);
+                    currentFolder?.Folders.Add(f);
+                    currentFolder = f;
+                }
+            }
+            else if (line.StartsWith("$ ls"))
+            {
+
+            }
+            else if (line.StartsWith("dir"))
+            {
+
+            }
+            else
+            {
+                var regex = new Regex(@"(\d+) (\S+)");
+                var match = regex.Match(line);
+                currentFolder.Files.Add(new File(match.Groups[2].Value, long.Parse(match.Groups[1].Value)));
+            }
+        }
+        var root = currentFolder;
+        while(root.Parent != null) { root = currentFolder.Parent; }
+        return root;
+    }
+
+
+
+    public long Part1(IEnumerable<string> input)
+    {
+        var folder = ParseInput(input);
+        return folder.GetChildFolders().Select(v =>v.GetSize()).Where(v => v <= 100000).Sum();
+    }
+    long Part2(IEnumerable<string> input) => 0;
 
 }
---
+++ UnitTests/Day7Tests.cs
@@ -0,0 +1,63 @@
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System;
+using AdventOfCode2022;
+using NUnit.Framework;
+using NUnit.Framework.Internal.Execution;
+using static AdventOfCode2022.Day7;
+using System.Linq;
+
+namespace UnitTests;
+
+public class Day7Tests
+{
+
+    private const string TestInput = @"$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k";
+
+    [TestCase("e", 584)]
+    [TestCase("a", 94853)]
+    [TestCase("d", 24933642)]
+    [TestCase("/", 48381165)]
+    public void MeasureDirectorySize(string folderName, long expectedSize)
+    {
+        var testInput = TestInput.Split("\r\n");
+        var solver = new Day7();
+        var root = solver.ParseInput(testInput);
+        var dict = root.GetChildFolders().ToDictionary(f => f.Name, f => f.GetSize());
+
+        Assert.That(dict[folderName], Is.EqualTo(expectedSize));
+    }
+    [Test]
+    public void Part1()
+    {
+        var testInput = TestInput.Split("\r\n");
+        var solver = new Day7();
+        var part1 = solver.Part1(testInput);
+
+        Assert.That(part1, Is.EqualTo(95437));
+    }
+    
+}
