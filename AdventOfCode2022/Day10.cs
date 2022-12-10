583d5d2a985cd5358ca1ee15087f30f9369dc1d3
Mark Heath <mark.heath@gmail.com>
2022-12-10 09:47:25 +0000

    day 10 part 1

--- AdventOfCode2022/Day10.cs
+++ AdventOfCode2022/Day10.cs
@@ -1,15 +1,70 @@
-﻿namespace AdventOfCode2022;
+﻿using System.Runtime.CompilerServices;
+
+namespace AdventOfCode2022;
 
 public class Day10 : ISolver
 {
-    public (string, string) ExpectedResult => ("", "");
+    
+    public (string, string) ExpectedResult => ("14060", "");
 
     public (string, string) Solve(string[] input)
     {
         return ($"{Part1(input)}", $"{Part2(input)}");
     }
 
-    long Part1(IEnumerable<string> input) => throw new NotImplementedException();
-    long Part2(IEnumerable<string> input) => throw new NotImplementedException();
+    public IEnumerable<int> Compute(IEnumerable<string> input)
+    {
+        int X = 1;
+        foreach (var inst in input)
+        {
+            if (inst == "noop")
+            {
+                yield return X;
+            }
+            if (inst.StartsWith("addx"))
+            {
+                yield return X;
+                yield return X;
+                X += int.Parse(inst[5..]);
+            }
+        }
+    }
+
+    public long Part1(IEnumerable<string> input)
+    {
+        var output = Compute(input).ToArray();
+        var targets = new[] { 20, 60, 100, 140, 180, 220 };
+        return targets.Select(t => output[t - 1] * t).Sum();
+        /*var X = 1;
+        var cycle = 1;
+        long signalStrength = 0;
+        var targetPos = 0;
+        foreach(var inst in input)
+        {
+            if (cycle >= targets[targetPos])
+            {
+                signalStrength += X * targets[targetPos];
+                targetPos++;
+                if (targetPos >= targets.Length) break;
+            }
+
+            if (inst == "noop")
+            {
+                cycle ++;
+            }
+            if (inst.StartsWith("addx"))
+            {
+                X += int.Parse(inst[5..]);
+                cycle += 2;
+            }
+        }
+        while(targetPos < targets.Length)
+        {
+            signalStrength += X * targets[targetPos];
+            targetPos++;
+        }
+        return signalStrength;*/
+    }
+    long Part2(IEnumerable<string> input) => 0;
 
 }
