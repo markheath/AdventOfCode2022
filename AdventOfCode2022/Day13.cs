f4e438a60e67c3038634d561716b2ced758d1996
Mark Heath <mark.heath@gmail.com>
2022-12-13 06:25:39 +0000

    Day 13 part 1 passes unit tests but wrong answer

--- AdventOfCode2022/Day13.cs
+++ AdventOfCode2022/Day13.cs
@@ -1,15 +1,171 @@
-﻿namespace AdventOfCode2022;
+﻿using System.Text;
+
+namespace AdventOfCode2022;
 
 public class Day13 : ISolver
 {
-    public (string, string) ExpectedResult => ("", "");
+    public (string, string) ExpectedResult => ("", ""); // 4683 wrong part 1 - too high
+
+
 
     public (string, string) Solve(string[] input)
     {
+        /* testing the parser - seems fine
+        var pairs = input.Chunk(3).Select((chunk, index) => new { Index = index + 1, First = Parse(chunk[0]), Second = Parse(chunk[1]) });
+        var sb = new StringBuilder();
+        foreach(var pair in pairs)
+        {
+            sb.AppendLine($"{pair.First}");
+            sb.AppendLine($"{pair.Second}");
+            sb.AppendLine();
+        }
+        File.WriteAllText(@"C:\Users\mheath\code\github\AdventOfCode2022\Input\Day13Parsed.txt", sb.ToString());*/
+
+
         return ($"{Part1(input)}", $"{Part2(input)}");
     }
 
-    long Part1(IEnumerable<string> input) => throw new NotImplementedException();
-    long Part2(IEnumerable<string> input) => throw new NotImplementedException();
+    public class PacketItem 
+    {
+
+    };
+    public class IntPacketItem : PacketItem
+    {
+        public IntPacketItem(string number)
+        {
+            Number = int.Parse(number);
+        }
+
+        public int Number { get; }
+
+        public override string ToString()
+        {
+            return Number.ToString();
+        }
+    }
+    public class ListPacketItem : PacketItem
+    {
+        public ListPacketItem(ListPacketItem parent)
+        {
+            List = new List<PacketItem>();
+            Parent = parent;
+        }
+
+        public List<PacketItem> List { get;  }
+        public ListPacketItem? Parent { get; }
+
+        public bool IsRightOrder(ListPacketItem other)
+        {
+
+            for(var index = 0; index < this.List.Count; index++)
+            {
+                var left = this.List[index];
+                // If the right list runs out of items first, the inputs are not in the right order.
+                if (index >= other.List.Count) return false;
+                var right = other.List[index];
+
+                if (left is IntPacketItem leftInt && right is IntPacketItem rightInt)
+                {
+                    // If both values are integers, the lower integer should come first.
+                    // If the left integer is lower than the right integer, the inputs are in the right order.
+
+                    if (leftInt.Number < rightInt.Number) return true;
+                    // If the left integer is higher than the right integer, the inputs are not in the right order.
+                    if (leftInt.Number > rightInt.Number) return false;
+                    // Otherwise, the inputs are the same integer; continue checking the next part of the input
+
+                }
+                else if (left is ListPacketItem leftList && right is ListPacketItem rightList)
+                {
+                    // If both values are lists, compare the first value of each list, then the second value, and so on.
+                    return leftList.IsRightOrder(rightList);
+                }
+                else 
+                {
+                    // If exactly one value is an integer, convert the integer to a list which contains that integer as its only value, then retry the comparison.
+                    if (left is IntPacketItem) // right must be a list packet
+                    {
+                        var leftList2 = new ListPacketItem(null);
+                        leftList2.List.Add(left);
+                        return leftList2.IsRightOrder((ListPacketItem)right);
+                    }
+                    else // left is a list, right is a number
+                    {
+                        var rightList2 = new ListPacketItem(null);
+                        rightList2.List.Add((IntPacketItem)right);
+                        return ((ListPacketItem)left).IsRightOrder(rightList2);
+                    }
+                }
+                
+            }
+            // If the left list runs out of items first, the inputs are in the right order.
+            return true; 
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("[");
+            foreach(var item in List)
+            {
+                sb.Append(item.ToString());
+                sb.Append(",");
+            }
+            if (List.Count > 0) sb.Length--;
+            sb.Append("]");
+            return sb.ToString();
+        }
+
+    }
+
+
+    public ListPacketItem Parse(string s)
+    {
+
+        ListPacketItem parent = new ListPacketItem(null);
+        ListPacketItem current = parent;
+        var currentNum = "";
+        for(var n = 1; n < s.Length; n++)
+        {
+            if (s[n] == '[')
+            {
+                var newList = new ListPacketItem(current);
+                current.List.Add(newList);
+                current = newList;
+            }
+            else if (s[n] == ']')
+            {
+                if (currentNum.Length > 0)
+                {
+                    current.List.Add(new IntPacketItem(currentNum));
+                    currentNum = "";
+                }
+                current = current.Parent;
+            }
+            else if (s[n] == ',')
+            {
+                if (currentNum.Length > 0)
+                {
+                    current.List.Add(new IntPacketItem(currentNum));
+                    currentNum = "";
+                }
+            }
+            else
+            {
+                currentNum += s[n];
+            }
+        }
+        return parent;
+    }
+
+    long Part1(IEnumerable<string> input)
+    {
+        return input.Chunk(3).Select((chunk, index) => new { Index = index + 1, First = Parse(chunk[0]), Second = Parse(chunk[1]) })
+            .Where(p => p.First.IsRightOrder(p.Second)).Sum(p => p.Index);
+    }
+    long Part2(IEnumerable<string> input)
+    {
+        return 0;
+    }
 
 }
---
+++ UnitTests/Day13Tests.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode2022;
+using NUnit.Framework;
+using static AdventOfCode2022.Day13;
+
+namespace UnitTests;
+
+public class Day13Tests
+{
+    private const string TestInput = @"[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]";
+
+    [Test]
+    public void Day13TestInput()
+    {
+        var testInput = TestInput.Split("\r\n");
+        var solver = new Day13();
+        var solution = solver.Solve(testInput);
+        Assert.AreEqual(("13", "0"), solution);
+    }
+
+    [Test]
+    public void Day13ComparisonsCorrect()
+
+    {
+        var testInput = TestInput.Split("\r\n");
+        var solver = new Day13();
+        var orders = testInput.Chunk(3).Select((chunk, index) => new { Index = index + 1, First = solver.Parse(chunk[0]), Second = solver.Parse(chunk[1]) })
+            .Select(p => p.First.IsRightOrder(p.Second));
+        Assert.AreEqual(new[] { true,true, false,true,false,true,false,false }, orders);
+    }
+
+
+    [Test]
+    public void Day13ParseBasicList()
+
+    {
+        var solver = new Day13();
+        var p = solver.Parse("[1,1,3,10,1]");
+        Assert.AreEqual(5, p.List.Count);
+        Assert.AreEqual(new List<int>() { 1,1,3,10,1}, p.List.Cast<IntPacketItem>().Select(n => n.Number));
+    }
+
+    [Test]
+    public void Day13ParseNestedList()
+
+    {
+        var solver = new Day13();
+        var p = solver.Parse("[[1],[2,3,4]]");
+        Assert.AreEqual(2, p.List.Count);
+        Assert.AreEqual(new List<int>() { 1, }, ((ListPacketItem)p.List[0]).List.Cast<IntPacketItem>().Select(n => n.Number));
+        Assert.AreEqual(new List<int>() { 2,3,4 }, ((ListPacketItem)p.List[1]).List.Cast<IntPacketItem>().Select(n => n.Number));
+    }
+
+    [Test]
+    public void Day13ParseEmptyList()
+
+    {
+        var solver = new Day13();
+        var p = solver.Parse("[[[]]]");
+        Assert.AreEqual(1, p.List.Count);
+        var firstChild = (ListPacketItem)p.List[0];
+        Assert.AreEqual(1, firstChild.List.Count);
+        var secondChild = (ListPacketItem)firstChild.List[0];
+        Assert.AreEqual(0, secondChild.List.Count);
+    }
+
+
+
+}
