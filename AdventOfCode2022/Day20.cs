6d110e3198e93988219762af99580a69122b4b5d
Mark Heath <mark.heath@gmail.com>
2022-12-20 13:33:11 +0000

    Made my linked list code a bit more generic for potential use in future

--- AdventOfCode2022/Day20.cs
+++ AdventOfCode2022/Day20.cs
@@ -11,14 +11,13 @@ public class Day20 : ISolver
 
     public long Part1(IEnumerable<string> input)
     {
-        var numbers = input.Select(long.Parse).ToList();
-        var allNodes = ToLinkedList(numbers).ToList();
+        var allNodes = LinkedListNode<long>.ToLinkedList(input.Select(long.Parse)).Enumerate().ToList();
         var zeroNode = allNodes.Single(n => n.Value == 0);
         Mix(allNodes);
         return SumCoordinates(zeroNode);
     }
 
-    private static void Mix(ICollection<Node<long>> allNodes)
+    private static void Mix(ICollection<LinkedListNode<long>> allNodes)
     {
         foreach (var originalNode in allNodes)
         {
@@ -31,8 +30,7 @@ public class Day20 : ISolver
     public long Part2(IEnumerable<string> input)
     {
         var decryptionKey = 811589153;
-        var numbers = input.Select(n => long.Parse(n) * decryptionKey).ToList();
-        var allNodes = ToLinkedList(numbers).ToList();
+        var allNodes = LinkedListNode<long>.ToLinkedList(input.Select(n => long.Parse(n) * decryptionKey)).Enumerate().ToList();
         var zeroNode = allNodes.Single(n => n.Value == 0);
         for (var n = 0; n < 10; n++)
         {
@@ -41,7 +39,7 @@ public class Day20 : ISolver
         return SumCoordinates(zeroNode);
     }
 
-    private static long SumCoordinates(Node<long> zeroNode)
+    private static long SumCoordinates(LinkedListNode<long> zeroNode)
     {
         long sum = 0;
         for (var n = 0; n < 3; n++)
@@ -51,97 +49,4 @@ public class Day20 : ISolver
         }
         return sum;
     }
-
-    private static IEnumerable<Node<long>> ToLinkedList(List<long> numbers)
-    {
-        List<Node<long>> allNodes = new List<Node<long>>();
-        Node<long>? prev = null;
-        foreach (var number in numbers)
-        {
-            var newNode = new Node<long>(number, prev);
-            allNodes.Add(newNode);
-            if (prev != null) { prev.Next = newNode; }
-            prev = newNode;
-        }
-        prev!.Next = allNodes[0]; // make it circular
-        allNodes[0].Prev = prev;
-        return allNodes;
-    }
-
-    class Node<T>
-    {
-        public Node<T> Skip(int steps)
-        {
-            var target = this;
-            for (var n = 0; n < Math.Abs(steps); n++)
-            {
-                target = steps > 0 ? target.Next : target.Prev;
-            }
-            return target;
-        }
-
-        public void Move(int steps)
-        {
-            if (steps == 0) return;
-            var startingPoint = steps > 0 ? this.Prev : this.Next;
-            // take myself out
-            Remove();
-
-            var target = startingPoint.Skip(steps);
-
-            // add myself in
-            if (steps > 0) InsertAfter(target);  
-            else InsertBefore(target);
-        }
-
-
-        public IEnumerable<Node<T>> Enumerate()
-        {
-            var t = this;
-            do
-            {
-                yield return t;
-                t = t.Next;
-            } while (t != this);
-        }
-
-        public void InsertAfter(Node<T> target)
-        {
-            this.Next = target.Next;
-            this.Prev = target;
-
-            target.Next.Prev = this;
-            target.Next = this;
-        }
-
-        public void InsertBefore(Node<T> target)
-        {
-            this.Next = target;
-            this.Prev = target.Prev;
-
-            target.Prev.Next = this;
-            target.Prev = this;
-        }
-
-        public void Remove()
-        {
-            // take myself out
-            this.Prev.Next = this.Next;
-            this.Next.Prev = this.Prev;
-            this.Prev = null;
-            this.Next = null;
-        }
-
-        public Node(T value, Node<T>? prev)
-        {
-            Value = value;
-            Prev = prev;
-        }
-
-        public Node<T> Prev { get; set; }
-        public Node<T> Next { get; set; }
-        public T Value { get; }
-    }
-
-
 }
---
+++ AdventOfCode2022/LinkedListNode.cs
@@ -0,0 +1,99 @@
+﻿namespace AdventOfCode2022;
+
+/// <summary>
+/// Basic linked list
+/// </summary>
+/// <typeparam name="T"></typeparam>
+class LinkedListNode<T>
+{
+    public LinkedListNode<T>? Prev { get; set; }
+    public LinkedListNode<T>? Next { get; set; }
+    public T Value { get; }
+
+    public LinkedListNode(T value, LinkedListNode<T>? prev)
+    {
+        Value = value;
+        Prev = prev;
+    }
+
+    public static LinkedListNode<T> ToLinkedList(IEnumerable<T> items, bool circular = true)
+    {
+        LinkedListNode<T>? head = null;
+        LinkedListNode<T>? tail = null;
+        foreach (var item in items)
+        {
+            var newNode = new LinkedListNode<T>(item, tail);
+            if (head == null) head = newNode;
+            if (tail != null) { tail.Next = newNode; }
+            tail = newNode;
+        }
+        if (head == null) throw new InvalidOperationException("no items");
+        if (circular)
+        {
+            tail!.Next = head; // make it circular
+            head.Prev = tail;
+        }
+        return head;
+    }
+
+    public LinkedListNode<T> Skip(int steps)
+    {
+        var target = this;
+        for (var n = 0; n < Math.Abs(steps); n++)
+        {
+            target = steps > 0 ? target.Next : target.Prev;
+        }
+        return target;
+    }
+
+    public void Move(int steps)
+    {
+        if (steps == 0) return;
+        var startingPoint = steps > 0 ? this.Prev : this.Next;
+        // take myself out
+        Remove();
+
+        var target = startingPoint.Skip(steps);
+
+        // add myself in
+        if (steps > 0) InsertAfter(target);
+        else InsertBefore(target);
+    }
+
+    public IEnumerable<LinkedListNode<T>> Enumerate()
+    {
+        var t = this;
+        do
+        {
+            yield return t;
+            t = t.Next;
+        } while (t != this && t != null);
+    }
+
+    public void InsertAfter(LinkedListNode<T> target)
+    {
+        this.Next = target.Next;
+        this.Prev = target;
+
+        target.Next.Prev = this;
+        target.Next = this;
+    }
+
+    public void InsertBefore(LinkedListNode<T> target)
+    {
+        this.Next = target;
+        this.Prev = target.Prev;
+
+        target.Prev.Next = this;
+        target.Prev = this;
+    }
+
+    public void Remove()
+    {
+        // take myself out
+        this.Prev.Next = this.Next;
+        this.Next.Prev = this.Prev;
+        this.Prev = null;
+        this.Next = null;
+    }
+}
