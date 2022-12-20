ff631f4574d6e477fff41b8633f37c033cd11d97
Mark Heath <mark.heath@gmail.com>
2022-12-20 10:22:41 +0000

    day 20 part 1

--- AdventOfCode2022/Day20.cs
+++ AdventOfCode2022/Day20.cs
@@ -2,14 +2,123 @@
 
 public class Day20 : ISolver
 {
-    public (string, string) ExpectedResult => ("", "");
+    public (string, string) ExpectedResult => ("11616", "");
 
     public (string, string) Solve(string[] input)
     {
         return ($"{Part1(input)}", $"{Part2(input)}");
     }
 
-    long Part1(IEnumerable<string> input) => throw new NotImplementedException();
-    long Part2(IEnumerable<string> input) => throw new NotImplementedException();
+    public long Part1(IEnumerable<string> input)
+    {
+        var numbers = input.Select(int.Parse).ToList();
+        var allNodes = ToLinkedList(numbers).ToList();
+        var zeroNode = allNodes.Single(n => n.Value == 0);
+        foreach (var originalNode in allNodes)
+        {
+            Console.WriteLine($"Moving {originalNode.Value}");
+            originalNode.Move(originalNode.Value);
+            Console.WriteLine(string.Join(',', allNodes[0].Enumerate().Select(n => n.Value)));
+            //break; 
+        }
+        // lazy!
+        var val1 = zeroNode.Skip(1000).Value;
+        var val2 = zeroNode.Skip(2000).Value;
+        var val3 = zeroNode.Skip(3000).Value;
+        return val1 + val2 + val3;
+    }
+
+    private static IEnumerable<Node<int>> ToLinkedList(List<int> numbers)
+    {
+        List<Node<int>> allNodes = new List<Node<int>>();
+        Node<int>? prev = null;
+        foreach (var number in numbers)
+        {
+            var newNode = new Node<int>(number, prev);
+            allNodes.Add(newNode);
+            if (prev != null) { prev.Next = newNode; }
+            prev = newNode;
+        }
+        prev!.Next = allNodes[0]; // make it circular
+        allNodes[0].Prev = prev;
+        return allNodes;
+    }
+
+    class Node<T>
+    {
+        public Node<T> Skip(int steps)
+        {
+            var target = this;
+            for (var n = 0; n < Math.Abs(steps); n++)
+            {
+                target = steps > 0 ? target.Next : target.Prev;
+            }
+            return target;
+        }
+
+        public void Move(int steps)
+        {
+            if (steps == 0) return;
+            var startingPoint = steps > 0 ? this.Prev : this.Next;
+            // take myself out
+            Remove();
+
+            var target = startingPoint.Skip(steps);
+
+            // add myself in
+            if (steps > 0) InsertAfter(target);  
+            else InsertBefore(target);
+        }
+
+
+        public IEnumerable<Node<T>> Enumerate()
+        {
+            var t = this;
+            do
+            {
+                yield return t;
+                t = t.Next;
+            } while (t != this);
+        }
+
+        public void InsertAfter(Node<T> target)
+        {
+            this.Next = target.Next;
+            this.Prev = target;
+
+            target.Next.Prev = this;
+            target.Next = this;
+        }
+
+        public void InsertBefore(Node<T> target)
+        {
+            this.Next = target;
+            this.Prev = target.Prev;
+
+            target.Prev.Next = this;
+            target.Prev = this;
+        }
+
+        public void Remove()
+        {
+            // take myself out
+            this.Prev.Next = this.Next;
+            this.Next.Prev = this.Prev;
+            this.Prev = null;
+            this.Next = null;
+        }
+
+        public Node(T value, Node<T>? prev)
+        {
+            Value = value;
+            Prev = prev;
+        }
+
+        public Node<T> Prev { get; set; }
+        public Node<T> Next { get; set; }
+        public T Value { get; }
+    }
+
+    long Part2(IEnumerable<string> input) => 0;
 
 }
---
+++ UnitTests/Day20Tests.cs
@@ -0,0 +1,49 @@
+using AdventOfCode2022;
+using NUnit.Framework;
+
+namespace UnitTests;
+
+public class Day20Tests
+{
+    private const string TestInput = @"1
+2
+-3
+3
+-2
+0
+4";
+
+    [Test]
+    public void Day20Part1TestInput()
+    {
+        var solver = new Day20();
+        var n = solver.Part1(TestInput.Split("\r\n"));
+        Assert.AreEqual(3, n);
+    }
+
+    /*
+     * Initial arrangement:
+1, 2, -3, 3, -2, 0, 4
+
+1 moves between 2 and -3:
+2, 1, -3, 3, -2, 0, 4
+
+2 moves between -3 and 3:
+1, -3, 2, 3, -2, 0, 4
+
+-3 moves between -2 and 0:
+1, 2, 3, -2, -3, 0, 4
+
+3 moves between 0 and 4:
+1, 2, -2, -3, 0, 3, 4
+
+-2 moves between 4 and 1:
+1, 2, -3, 0, 3, 4, -2
+
+0 does not move:
+1, 2, -3, 0, 3, 4, -2
+
+4 moves between -3 and 0:
+1, 2, -3, 4, 0, 3, -2
+     */
+}
