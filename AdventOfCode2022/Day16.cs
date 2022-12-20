08ae9804fe50736b0af8fe5ab0b067f4621243ee
Mark Heath <mark.heath@gmail.com>
2022-12-20 10:29:59 +0000

    modulo arithmetic optimise part 1

--- AdventOfCode2022/Day16.cs
+++ AdventOfCode2022/Day16.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 using SuperLinq;
 
@@ -10,6 +11,7 @@ public class Day16 : ISolver
 
     public (string, string) Solve(string[] input)
     {
+        //return Better(input);
         var valves = ParseValves(input);
         var sw = Stopwatch.StartNew();        
         var part1 = FindMaxFlow2(valves, new HashSet<string>() { "AA" }, ("AA", 30), ("XXX", 0));
@@ -158,9 +160,82 @@ public class Day16 : ISolver
             //if (max > bestSoFar)
             //{ bestSoFar = max; Console.WriteLine(bestSoFar); }
         }
+
+        // note: might need this to be correct on some inputs - elephant's help isn't always what we want
+        // if (other.pos != "XXX") max = Math.Max(FindMaxFlow2(valves, openedValves, ("XXX", 0), other), max);
+
         cache[cacheKey] = max;
         return max;
     }
 
-}
+    // since my day 16 solution still took 1.5mins to run, thought I'd learn from someone else
+    // this elegant Python solution (by 4HbQ) is not that dissimilar from my final approach
+    // this one runs in about 20s
+    // https://www.reddit.com/r/adventofcode/comments/zn6k1l/comment/j0fti6c/?utm_source=share&utm_medium=web2x&context=3
+    public (string,string) Better(string[] input)
+    {
+        var regex = new Regex(@"Valve (\w+) .*=(\d*); .* valves? (.*)");
+        var V = new HashSet<string>(); // valve names
+        var F = new Dictionary<string, int>(); // flow
+        var D = new DefaultDictionary<(string, string), int>(1000); // distances
+        foreach (var match in input.Select(line => regex.Match(line)))
+        {
+            var v = match.Groups[1].Value;
+            var f = match.Groups[2].Value;
+            var us = match.Groups[3].Value;
+            V.Add(v); // store node
+            if (f != "0") F[v] = int.Parse(f); // store flow
+            foreach (var u in us.Split(", "))
+            {
+                D[(u, v)] = 1; //  store dist
+            }
+        }
 
+        // floyd-warshall
+        foreach (var (k, i, j) in V.Cartesian(V, V, (a, b, c) => (a, b, c)))
+            D[(i, j)] = Math.Min(D[(i, j)], D[(i, k)] + D[(k, j)]);
+
+        //@functools.cache
+        int Search(int t, Dictionary<string, int> cache, string u, 
+            ImmutableHashSet<string>vs, bool e = false) 
+        {
+            var cacheKey = $"{t}:{u}:{string.Join("", vs)}:{e}";
+            if (cache.TryGetValue(cacheKey, out var val)) return val;
+            var max = 
+                vs.Where(v => D[(u, v)] < t)
+                   .Select(v => F[v] * (t - D[(u, v)] - 1) + Search(t - D[(u, v)] - 1, cache, v, vs.Remove(v), e))
+                .Append(e ? Search(26, cache, "AA", vs) : 0 )
+                .Max();
+            cache[cacheKey] = max;
+            return max;
+            // how part 2 works: we're essentially solving for 1 player, but after every valve, working out what would happen if the elephant did all the rest of them (starting from beginning), resulting in all possible partitionings of valves between player and elephant
+        }
+        var p1 = Search(30, new(), "AA", ImmutableHashSet<string>.Empty.Union(F.Keys));
+        var p2 = Search(26, new(), "AA", ImmutableHashSet<string>.Empty.Union(F.Keys), e: true);
+        return (p1.ToString(), p2.ToString());
+
+        /* import collections as c, itertools, functools, re
+
+ r = r'Valve (\w+) .*=(\d*); .* valves? (.*)'
+
+ V, F, D = set(), dict(), c.defaultdict(lambda: 1000)
+
+ for v, f, us in re.findall(r, open('in.txt').read()):
+     V.add(v)                                  # store node
+     if f != '0': F[v] = int(f)                # store flow
+     for u in us.split(', '): D[u,v] = 1       # store dist
+
+ for k, i, j in itertools.product(V, V, V):    # floyd-warshall
+     D[i,j] = min(D[i,j], D[i,k] + D[k,j])
+
+ @functools.cache
+ def search(t, u='AA', vs=frozenset(F), e=False):
+     return max(
+        
+    [F[v] * (t-D[u,v]-1) + search(t-D[u,v]-1, v, vs-{v}, e) for v in vs if D[u,v]<t] 
+        + [search(26, vs=vs) if e else 0])
+
+ print(search(30), search(26, e=True))
+        */
+    }
+}
