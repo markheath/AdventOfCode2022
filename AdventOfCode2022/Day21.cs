3d1f505fffb3e4cb4a1bdad4fef7b6f667bd2dd3
Mark Heath <mark.heath@gmail.com>
2022-12-21 07:13:06 +0000

    Day 21 part 1

--- AdventOfCode2022/Day21.cs
+++ AdventOfCode2022/Day21.cs
@@ -2,14 +2,40 @@
 
 public class Day21 : ISolver
 {
-    public (string, string) ExpectedResult => ("", "");
+    public (string, string) ExpectedResult => ("276156919469632", "");
 
     public (string, string) Solve(string[] input)
     {
         return ($"{Part1(input)}", $"{Part2(input)}");
     }
 
-    long Part1(IEnumerable<string> input) => throw new NotImplementedException();
-    long Part2(IEnumerable<string> input) => throw new NotImplementedException();
+    long Part1(IEnumerable<string> input)
+    {
+        var calcs = input.Select(i => new { id = i[0..4], val = i[6..] })
+               .Where(m => m.val.Length == 11).ToDictionary(m => m.id, m => m.val.Split(' '));
+        var answers = input.Select(i => new { id = i[0..4], val = i[6..] })
+               .Where(m => m.val.Length < 11).ToDictionary(m => m.id, m => long.Parse(m.val));
+        while(!answers.ContainsKey("root"))
+        {
+            bool solved = false;
+            foreach(var calc in calcs.Where(c => !answers.ContainsKey(c.Key) && answers.ContainsKey(c.Value[0]) && answers.ContainsKey(c.Value[2])))
+            {
+
+                answers[calc.Key] = calc.Value[1] switch
+                {
+                    "+" => answers[calc.Value[0]] + answers[calc.Value[2]],
+                    "-" => answers[calc.Value[0]] - answers[calc.Value[2]],
+                    "/" => answers[calc.Value[0]] / answers[calc.Value[2]],
+                    "*" => answers[calc.Value[0]] * answers[calc.Value[2]],
+                    _ => throw new InvalidOperationException("Unknown operator")
+                };
+                solved = true;
+            }
+            if (!solved)
+                throw new InvalidOperationException("failed to solve any calc this round");
+        }
+        return answers["root"];
+    }
+    long Part2(IEnumerable<string> input) => 0;
 
 }
---
+++ UnitTests/Day21Tests.cs
@@ -0,0 +1,31 @@
+using AdventOfCode2022;
+using NUnit.Framework;
+
+namespace UnitTests;
+
+public class Day21Tests
+{
+    private const string TestInput = @"root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32";
+
+    [Test]
+    public void Day21Part1TestInput()
+    {
+        var solver = new Day21();
+        var n = solver.Solve(TestInput.Split("\r\n"));
+        Assert.AreEqual(("152","0"), n);       
+    }
+}
