a06e561b1f759287ffb0b840876e25e0dd32f03b
Mark Heath <mark.heath@gmail.com>
2022-12-22 12:09:43 +0000

    refactoring part 1

--- AdventOfCode2022/Day22.cs
+++ AdventOfCode2022/Day22.cs
@@ -11,45 +11,34 @@ public class Day22 : ISolver
         return ($"{Part1(input)}", $"{Part2(input)}");
     }
 
+    private static readonly Coord[] directions = new Coord[] { (1, 0), (0, 1), (-1, 0), (0, -1) };
+
     long Part1(string[] input)
     {
-        var maxWidth = input.Take(input.Length-1).Max(line => line.Length);
+        var grid = ParseGrid(input, out var currentPos);
+        Console.WriteLine($"starting at {currentPos}");
+        // Initially, you are facing to the right
+        // directions clockwise go right, down, left, up
+        var directionIndex = 0;
+        var instructions = Regex.Matches(input.Last(), "[RL]|(\\d+)").Select(m => m.Value);
+        var teleports = GenerateTeleports(grid);
+
+        (currentPos, directionIndex) = FollowInstructions(grid, directionIndex, instructions, currentPos, teleports);
+        return CalculatePassword(directionIndex, currentPos);
+    }
+
+    private static Grid<char> ParseGrid(string[] input, out Coord startPos)
+    {
+        var maxWidth = input.Take(input.Length - 1).Max(line => line.Length);
         var gridLines = input.TakeWhile(line => !String.IsNullOrEmpty(line)).Select(line => line.PadRight(maxWidth)).ToArray();
         var grid = Grid<char>.ParseToGrid(gridLines, c => c);
         // You begin the path in the leftmost open tile of the top row of tiles.
-        var startPos = grid.LineOut((0, 0), (1, 0)).First(c => grid[c] == '.');
-        Console.WriteLine($"starting at {startPos}");
-        // Initially, you are facing to the right
-        // directions clockwise go right, down, left, up
-        var directions = new Coord[] { (1, 0), (0, 1), (-1, 0), (0, -1) };
-        var directionIndex = 0;
-        var instructions = Regex.Matches(input.Last(), "[RL]|(\\d+)").Select(m => m.Value);
-        var currentPos = startPos;
-        var teleports = new Dictionary<(Coord, Coord), (Coord, Coord)>();
-        
-        // do all the step off the right edge first
-        for(var y = 0; y < grid.Height; y++)
-        {
-            var minX = Enumerable.Range(0, grid.Width).First(x => grid[(x, y)] != ' ');
-            var maxX = Enumerable.Range(0, grid.Width).Last(x => grid[(x, y)] != ' ');
+        startPos = grid.LineOut((0, 0), (1, 0)).First(c => grid[c] == '.');
+        return grid;
+    }
 
-            // step left off left edge, appear on right, still facing left
-            teleports[((minX, y), (minX - 1, y))] = ((maxX, y), (-1, 0));
-            // step right off right edge, appear on left, still facing right
-            teleports[((maxX, y), (maxX + 1,y))] = ((minX, y), (1, 0));
-        }
-        for (var x = 0; x < grid.Width; x++)
-        {
-            var minY = Enumerable.Range(0, grid.Height).First(y => grid[(x, y)] != ' ');
-            var maxY = Enumerable.Range(0, grid.Height).Last(y => grid[(x, y)] != ' ');
-
-            // step up off top edge, appear at bottom, still facing up
-            teleports[((x, minY), (x, minY - 1))] = ((x, maxY), (0, -1));
-            // step down off bottom edge, appear at top, still facing down
-            teleports[((x, maxY), (x, maxY + 1))] = ((x, minY), (0, 1));
-        }
-
-
+    private static (Coord,int) FollowInstructions(Grid<char> grid, int directionIndex, IEnumerable<string> instructions, Coord currentPos, Dictionary<(Coord, Coord), (Coord, Coord)> teleports)
+    {
         foreach (var instruction in instructions)
         {
             if (instruction == "R")
@@ -68,7 +57,7 @@ public class Day22 : ISolver
                 {
                     var delta = directions[directionIndex];
                     var nextPos = currentPos + delta;
-                    if (teleports.ContainsKey((currentPos,nextPos)))
+                    if (teleports.ContainsKey((currentPos, nextPos)))
                     {
                         (nextPos, var newDelta) = teleports[(currentPos, nextPos)];
                         directionIndex = Array.IndexOf(directions, newDelta);
@@ -93,12 +82,45 @@ public class Day22 : ISolver
             }
 
         }
+        return (currentPos, directionIndex);
+    }
+
+    private static int CalculatePassword(int directionIndex, Coord currentPos)
+    {
         var row = currentPos.Y + 1;
         var column = currentPos.X + 1;
         var facing = directionIndex;
         var password = 1000 * row + 4 * column + facing;
         return password;
+    }
 
+    private static Dictionary<(Coord, Coord), (Coord, Coord)> GenerateTeleports(Grid<char> grid)
+    {
+        var teleports = new Dictionary<(Coord, Coord), (Coord, Coord)>();
+
+        // do all the step off the right edge first
+        for (var y = 0; y < grid.Height; y++)
+        {
+            var minX = Enumerable.Range(0, grid.Width).First(x => grid[(x, y)] != ' ');
+            var maxX = Enumerable.Range(0, grid.Width).Last(x => grid[(x, y)] != ' ');
+
+            // step left off left edge, appear on right, still facing left
+            teleports[((minX, y), (minX - 1, y))] = ((maxX, y), (-1, 0));
+            // step right off right edge, appear on left, still facing right
+            teleports[((maxX, y), (maxX + 1, y))] = ((minX, y), (1, 0));
+        }
+        for (var x = 0; x < grid.Width; x++)
+        {
+            var minY = Enumerable.Range(0, grid.Height).First(y => grid[(x, y)] != ' ');
+            var maxY = Enumerable.Range(0, grid.Height).Last(y => grid[(x, y)] != ' ');
+
+            // step up off top edge, appear at bottom, still facing up
+            teleports[((x, minY), (x, minY - 1))] = ((x, maxY), (0, -1));
+            // step down off bottom edge, appear at top, still facing down
+            teleports[((x, maxY), (x, maxY + 1))] = ((x, minY), (0, 1));
+        }
+
+        return teleports;
     }
 
     private static Coord MoveThroughEmptySpace(Grid<char> grid, Coord delta, Coord nextPos)
