593d112897b338ff206d2443b8082c46e1eecd6e
Mark Heath <mark.heath@gmail.com>
2022-12-24 13:48:55 +0000

    day 24 refactor 1

--- AdventOfCode2022/Day24.cs
+++ AdventOfCode2022/Day24.cs
@@ -13,25 +13,31 @@ public class Day24 : ISolver
 
         entrance = (startX, -1);
         exit = (exitX, input.Length - 2);
-        var innerWidth = input[0].Length - 2;
-        int innerHeight = input.Length - 2;
-        var bestSoFar = 400; //innerWidth * innerHeight; // set a sensible upper bound
+        innerWidth = input[0].Length - 2;
+        innerHeight = input.Length - 2;
+        var upperLimit = 400; // pick a max depth to start with
+        var bestSoFar = upperLimit; //innerWidth * innerHeight; // set a sensible upper bound
         Console.WriteLine($"upper bound {bestSoFar}");
-        Seek(entrance, exit, ref bestSoFar, 0, innerWidth, innerHeight);
+        Seek(entrance, exit, ref bestSoFar, 0);
         var part1 = bestSoFar;
         Console.WriteLine($"reached goal at {part1}");
-        bestSoFar += 400;
+        bestSoFar += upperLimit;
         seen.Clear();
-        Seek(exit, entrance, ref bestSoFar, part1, innerWidth, innerHeight);
+        Seek(exit, entrance, ref bestSoFar, part1);
         var gotBackBy = bestSoFar;
         Console.WriteLine($"got back to start at {gotBackBy} ({gotBackBy - part1})");
-        bestSoFar += 400;
+        bestSoFar += upperLimit;
         seen.Clear();
-        Seek(entrance, exit, ref bestSoFar, gotBackBy, innerWidth, innerHeight);
+        Seek(entrance, exit, ref bestSoFar, gotBackBy);
         var part2 = bestSoFar;
         Console.WriteLine($"returned to target at {part2} ({part2 - gotBackBy})");
         return ($"{part1}", $"{part2}");
     }
+
+    /*int Seek(Coord startPos, Coord targetPos, int startTime)
+    {
+
+    }*/
 
     void CacheHurricanes(int t, IEnumerable<Hurricane> hurricanes)
     {
@@ -41,10 +47,12 @@ public class Day24 : ISolver
 
     private Coord entrance;
     private Coord exit;
+    private int innerWidth;
+    private int innerHeight;
     private Dictionary<int, (HashSet<Coord>, IEnumerable<Hurricane>)> hurricaneCache = new();
-    private HashSet<(int, Coord)> seen = new(); // not sure if needed, but just in case
+    private HashSet<(int, Coord)> seen = new();
 
-    private void Seek(Coord currentPos, Coord targetPos, ref int bestSoFar, int t, int innerWidth, int innerHeight)
+    private void Seek(Coord currentPos, Coord targetPos, ref int bestSoFar, int t)
     {
         t++;
         if (seen.Contains((t, currentPos))) return;
@@ -54,7 +62,7 @@ public class Day24 : ISolver
             // abandon this route
             return;
         }
-        if (!hurricaneCache.ContainsKey(t)) CacheHurricanes(t, MoveHuricanes(hurricaneCache[t-1].Item2, innerWidth, innerHeight));
+        if (!hurricaneCache.ContainsKey(t)) CacheHurricanes(t, MoveHuricanes(hurricaneCache[t-1].Item2));
         // to try first right, then down, then stay put, then left
         var deltas = new Coord[] { (1, 0), (0, 1), (-1,0), (0,-1), (0, 0)};
         var blockedByHurricanes = hurricaneCache[t].Item1;
@@ -74,7 +82,7 @@ public class Day24 : ISolver
             // special case - entrance and exit squares are always free
             if (isBlocked && (newPos == entrance || newPos == exit)) isBlocked = false;
             if (!isBlocked)
-                Seek(newPos, targetPos, ref bestSoFar, t, innerWidth, innerHeight);
+                Seek(newPos, targetPos, ref bestSoFar, t);
         }
     }
 
@@ -93,7 +101,7 @@ public class Day24 : ISolver
         }
     }
 
-    public IEnumerable<Hurricane> MoveHuricanes(IEnumerable<Hurricane> hurricanes, int width, int height)
+    public IEnumerable<Hurricane> MoveHuricanes(IEnumerable<Hurricane> hurricanes)
     {
         foreach(var h in hurricanes)
         {
@@ -106,10 +114,10 @@ public class Day24 : ISolver
                 _ => throw new InvalidOperationException("Unknown hurricane direction")
             };
             var (x,y) = h.pos + delta;
-            if (x < 0) x = width - 1;
-            if (x >= width) x = 0;
-            if (y < 0) y = height - 1;
-            if (y >= height) y = 0;
+            if (x < 0) x = innerWidth - 1;
+            if (x >= innerWidth) x = 0;
+            if (y < 0) y = innerHeight - 1;
+            if (y >= innerHeight) y = 0;
             yield return new Hurricane(h.dir, (x, y));
         }
     }
